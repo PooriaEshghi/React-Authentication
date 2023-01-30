5fb453b71f8a0601133b242e59579ca755bbf9de
pooria eshghi malayeri <100807060+PooriaEshghi@users.noreply.github.com>
2023-01-30 16:54:37 +0100

    Initial commit

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useAuth } from "../context/AuthContext";
+
+const Dashboard = () => {
+  const { authUser, setAuthUser, isLoggedIn, setIsLoggedIn } = useAuth();
+  const logIn = (e) => {
+    e.preventDefault();
+    setIsLoggedIn(true);
+    setAuthUser({
+      Name: "Pooria",
+    });
+  };
+  const logOut = (e) => {
+    e.preventDefault();
+    setIsLoggedIn(false);
+    setAuthUser(null);
+  };
+  return (
+    <>
+      <span>User is currently: {isLoggedIn ? "Logged-In" : "Logged Out"}.</span>
+      <br />
+      {isLoggedIn ? <span>User name:{authUser.Name}</span> : null}
+      <br />
+      {isLoggedIn ? (
+        <button onClick={(e) => logOut(e)}>Log Out</button>
+      ) : (
+        <button onClick={(e) => logIn(e)}>Log In</button>
+      )}
+    </>
+  );
+};
+
+export default Dashboard;
